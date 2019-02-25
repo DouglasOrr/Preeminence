9ed510754ce185ee761f594bb75ea3c03bbe7d12
Douglas Orr <douglas.orr99@gmail.com>
2019-02-25 13:15:46 +0000

    Add start script for using the published Docker image

---
+++ run_notebook.sh
@@ -0,0 +1,14 @@
+NAME=${USER}-preem
+PORT=8888
+
+docker rm -f ${NAME} &> /dev/null || true
+docker run -d --name ${NAME} -p ${PORT}:${PORT} -e PYTHONPATH=/preem -v `pwd`:/preem -w /preem douglasorr/preeminence jupyter notebook --allow-root --port ${PORT} --ip 0.0.0.0 > /dev/null
+
+sleep 3
+
+TOKEN=$(docker logs ${NAME} 2>&1 | grep -oP '(?<=token=)(.+)$' | head -1)
+echo "See:"
+echo "    http://localhost:${PORT}/notebooks/Tutorial.ipynb?token=${TOKEN}"
+echo
+echo "When you want to stop your server:"
+echo "    docker rm -f ${NAME}"
