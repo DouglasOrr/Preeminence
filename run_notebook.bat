f818558d9e061d316f4835f83c854d5d7cdf4617
Douglas Orr <douglas.orr99@gmail.com>
2019-03-03 03:29:20 +0000

    Improve Windows run script (run_notebook.bat).

--- run_notebook.bat
+++ run_notebook.bat
@@ -1,3 +1,10 @@
+SET PREEM_NOTEBOOK=%USERNAME%-preem
+
 docker pull douglasorr/preeminence
-docker rm -f preem
-docker run --rm -it --name preem -p 8888:8888 -e PYTHONPATH=/preem douglasorr/preeminence jupyter notebook --allow-root --port 8888 --ip 0.0.0.0
+docker rm -f %PREEM_NOTEBOOK%
+docker run -d --name %PREEM_NOTEBOOK% -p 8888:8888 -e PYTHONPATH=/preem -v %cd%:/preem -w /preem douglasorr/preeminence jupyter notebook --allow-root --port 8888 --ip 0.0.0.0
+
+ping 127.0.0.1 -n 3 > nul
+docker logs %PREEM_NOTEBOOK%
+ECHO "Your notebook is running at http://localhost:8888/notebooks/Tutorial.ipynb ; see login token listed above"
+ECHO "When you want to stop your server, run: docker rm -f %PREEM_NOTEBOOK%"
